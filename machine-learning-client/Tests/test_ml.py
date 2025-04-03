284e1ee39de94b9a4a60f8b5ac94cdef7d0c82d0
RyanH0417 <2371161067@qq.com>
2025-04-03 12:40:28 -0400

    remove test

--- machine-learning-client/Tests/test_ml.py
+++
@@ -1,9 +0,0 @@
-"""
-This module contains tests for the ML client.
-"""
-def testml():
-    """
-    Simple test that always passes.
-    """
-    assert True
-    
--- web-app/Tests/test_app.py
+++
@@ -1,8 +0,0 @@
-"""
-This module contains tests for the web-app.
-"""
-def testapp():
-    """
-    Simple test that always passes.
-    """
-    assert True
